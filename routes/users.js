fe533c3bf751cc495b314151a585920b638b413b
gerardo agustin <gerardoagustin@hotmail.com>
2020-07-13 21:28:02 -0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+import {
+  createUser,
+  getUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/users.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+
+router.post("/", createUser);
+
+router.get("/:id", getUser);
+
+router.delete("/:id", deleteUser);
+
+router.patch("/:id", updateUser);
+
+export default router;
